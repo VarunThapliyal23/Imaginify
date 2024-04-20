14eac221c342e983837f6f80b8233b655c9fabc6
Varun Thapliyal <thapliyalvarun23@gmail.com>
2024-04-20 19:40:35 +0530

    initial commit

---
+++ lib/database/mongoose.ts
@@ -0,0 +1,31 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const MONODB_URL = process.env.MONGODB_URI;
+
+interface MongooseConnection{
+    conn: Mongoose | null;
+    promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConnection = (global as any).mongoose
+
+if(!cached) {
+    cached = (global as any).mongoose = {
+        conn : null, promise: null
+    }
+}
+
+export  const connectToDatabase = async () => {
+    if(cached.conn) return cached.conn;
+
+    if(!MONODB_URL) throw new Error( 'Missing MongoDB URL');
+    
+    cached.promise = cached.promise || 
+    mongoose.connect(MONODB_URL, {
+        dbName:'imaginify', bufferCommands: false     
+    })
+
+   cached.conn = await cached.promise;
+   
+   return cached.conn;
+}
